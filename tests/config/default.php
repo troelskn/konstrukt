c00d2e436048eee048bc35f5a038414577dffae3
Troels Knak-Nielsen <troelskn@gmail.com>
2007-06-20 09:29:46 +0000

    modified paths to match

--- tests/config/default.php
+++ tests/config/default.php
@@ -1,7 +1,7 @@
 <?php
-$path_simpletest = dirname(__FILE__).'/../../../simpletest/';
+$path_simpletest = dirname(__FILE__).'/../../../../simpletest/';
 $test_case_dir = dirname(__FILE__).'/../cases/';
-$test_server_url = "http://localhost/konstrukt/tests/cases/web_tests/";
+$test_server_url = "http://localhost/konstrukt/trunk/tests/cases/web_tests/";
 $dependencies = Array("../std.inc.php");
 $title = "All Tests";
 ?>
