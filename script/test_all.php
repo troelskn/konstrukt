21121c2e0c11687bdbf842a1e7d220a36c20c314
Lars Olesen <lars@intraface.dk>
2012-07-30 12:20:26 +0200

    Made sure that only test files with the proper naming scheme is run

--- script/test_all.php
+++ script/test_all.php
@@ -33,7 +33,7 @@ function verbose($str) {
 $passes = 0;
 $failures = 0;
 $runs = 0;
-foreach (find_files(getcwd(), '/test\.php$/') as $filename) {
+foreach (find_files(getcwd(), '/\.test\.php$/') as $filename) {
   chdir(dirname($filename));
   $path = realpath(dirname(__FILE__) . '/../lib') . PATH_SEPARATOR . ini_get("include_path");
   $xml = shell_exec('php -d include_path=' . escapeshellarg($path) . ' ' . escapeshellarg(basename($filename)) . ' -x');
